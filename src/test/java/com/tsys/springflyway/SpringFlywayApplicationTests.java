3f3c1d8ff6e86ef0aad702ea70121c3eb2f3699c
Dhaval Dalal <dalaldhaval05@gmail.com>
2024-04-03 11:40:28 +0530

    Added a smoke test

--- src/test/java/com/tsys/springflyway/SpringFlywayApplicationTests.java
+++ src/test/java/com/tsys/springflyway/SpringFlywayApplicationTests.java
@@ -1,13 +1,34 @@
 package com.tsys.springflyway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.sql.DataSource;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 class SpringFlywayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private ApplicationContext context;
 
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void contextLoads() throws SQLException {
+		assertNotNull(context);
+		assertNotNull(dataSource);
+		final String url = dataSource.getConnection().getMetaData().getURL();
+		assertThat(url, is("jdbc:mysql://localhost:3306/flywaydemo"));
+	}
 }
